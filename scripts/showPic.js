7bedf78928dc2b7ca5923f4a4add0ff3b4eaa294
slkk <644187060@qq.com>
2018-03-26 13:52:02 +0800

    chapter 4 complete

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,4 +1,14 @@
 function showPic(whichPic){
   var source = whichPic.getAttribute("href");
   document.getElementById("placeHolder").setAttribute("src",source);
+  var title = whichPic.getAttribute("title");
+  var description = document.getElementById("description");
+  description.firstChild.nodeValue = title;
 }
+
+function countBodyChildren(){
+  var body_element = document.getElementsByTagName("body")[0];
+  for(var i =1;i<body_element.length;i++){}
+  alert(body_element.childNodes.length)
+}
+window.onload = countBodyChildren;
